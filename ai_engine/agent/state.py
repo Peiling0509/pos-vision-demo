81d9c16fa933508c9d35db6d70558083b8a93343
Peiling0509 <130948548+Peiling0509@users.noreply.github.com>
2026-07-27 13:44:58 +0800

    Delete ai_engine directory

--- ai_engine/agent/state.py
+++
@@ -1,7 +0,0 @@
-from typing import TypedDict, Annotated
-from langgraph.graph.message import add_messages
-
-class AgentState(TypedDict):
-    # Annotated[list, add_messages]
-    # auto add the new message to list instead of overlay it
-    messages: Annotated[list, add_messages]
